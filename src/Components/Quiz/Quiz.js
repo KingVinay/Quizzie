cb6c7eba5911c61fb5be93ba6996e2ee0eca52c9
Vinay Singh <vinaysthakur15@gmail.com>
2024-05-31 21:40:07 +0530

    Quiz Interface re rendering handled with result page configured

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -2,13 +2,17 @@ import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import styles from "./Quiz.module.css";
+import Trophy from "../../Assets/Trophy.png";
 
 const Quiz = () => {
   const { quizId } = useParams();
-  const [quizData, setQuizData] = useState(null);
+  const [quizData, setQuizData] = useState("");
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedOptions, setSelectedOptions] = useState([]);
   const [timer, setTimer] = useState(null);
+  const [resultData, setResultData] = useState("");
+  const [result, setResult] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   useEffect(() => {
     const fetchQuizData = async () => {
@@ -45,26 +49,34 @@ const Quiz = () => {
   }, [currentQuestionIndex, quizData]);
 
   const handleSubmit = useCallback(async () => {
+    if (isSubmitted) {
+      return;
+    }
+    console.log({ selectedOptions: selectedOptions });
     try {
-      const response = await axios.post(
-        `http://localhost:4000/api/quiz/submit/${quizId}`,
-        {
-          answers: selectedOptions,
-        }
-      );
-      setQuizData({ ...quizData, results: response.data });
+      const response = await axios({
+        method: "post",
+        url: `http://localhost:4000/api/quiz/submit/${quizId}`,
+        data: {
+          selectedOptions: selectedOptions,
+        },
+      });
+      setResultData({ results: response.data });
+      setResult(true);
+      setIsSubmitted(true);
     } catch (error) {
       console.error("Error submitting quiz:", error);
     }
-  }, [quizData, quizId, selectedOptions]);
+  }, [quizId, selectedOptions, isSubmitted]);
 
   const handleNext = useCallback(() => {
     if (currentQuestionIndex < quizData.questions.length - 1) {
       setCurrentQuestionIndex((prev) => prev + 1);
+      setTimer(null);
     } else {
       handleSubmit();
     }
-  }, [currentQuestionIndex, handleSubmit, quizData]);
+  }, [currentQuestionIndex, handleSubmit, quizData.questions]);
 
   useEffect(() => {
     if (timer === 0) {
@@ -95,59 +107,68 @@ const Quiz = () => {
 
   return (
     <div className={styles.quizComponent}>
-      <div className={styles.quizContainer}>
-        <div className={styles.quizHeader}>
-          <div>{`0${currentQuestionIndex + 1} / 0${
-            quizData.questions.length
-          }`}</div>
-          {quizData.quizType === "q&a" && currentQuestion.timer && (
-            <div className={styles.timer}>{`00:${
-              timer < 10 ? `0${timer}` : timer
-            }s`}</div>
-          )}
-        </div>
-        <div className={styles.question}>{currentQuestion.questionName}</div>
-        <div className={styles.options}>
-          {currentQuestion.options.map((option, index) => (
-            <div
-              key={index}
-              className={`${styles.option} ${
-                selectedOptions[currentQuestionIndex]?.selectedOptionId ===
-                option._id
-                  ? styles.selectedOption
-                  : ""
-              }`}
-              onClick={() => handleOptionSelect(index, option._id)}
-            >
-              {currentQuestion.optionType === "text" && (
-                <span>{option.text}</span>
-              )}
-              {currentQuestion.optionType === "image url" && (
-                <img src={option.imageUrl} alt="option" />
-              )}
-              {currentQuestion.optionType === "text and image url" && (
-                <div className={styles.optionContent}>
+      {!result && (
+        <div className={styles.quizContainer}>
+          <div className={styles.quizHeader}>
+            <div>{`0${currentQuestionIndex + 1} / 0${
+              quizData.questions.length
+            }`}</div>
+            {quizData.quizType === "q&a" && currentQuestion.timer && (
+              <div className={styles.timer}>{`00:${
+                timer < 10 ? `0${timer}` : timer
+              }s`}</div>
+            )}
+          </div>
+          <div className={styles.question}>{currentQuestion.questionName}</div>
+          <div className={styles.options}>
+            {currentQuestion.options.map((option, index) => (
+              <div
+                key={index}
+                className={`${styles.option} ${
+                  selectedOptions[currentQuestionIndex]?.selectedOptionId ===
+                  option._id
+                    ? styles.selectedOption
+                    : ""
+                }`}
+                onClick={() => handleOptionSelect(index, option._id)}
+              >
+                {currentQuestion.optionType === "text" && (
                   <span>{option.text}</span>
+                )}
+                {currentQuestion.optionType === "image url" && (
                   <img src={option.imageUrl} alt="option" />
-                </div>
-              )}
-            </div>
-          ))}
+                )}
+                {currentQuestion.optionType === "text and image url" && (
+                  <div className={styles.optionContent}>
+                    <span>{option.text}</span>
+                    <img src={option.imageUrl} alt="option" />
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <button className={styles.nextButton} onClick={handleNext}>
+            {isLastQuestion ? "Submit" : "Next"}
+          </button>
         </div>
-        <button className={styles.nextButton} onClick={handleNext}>
-          {isLastQuestion ? "Submit" : "Next"}
-        </button>
-      </div>
-      {quizData.results && (
+      )}
+      {resultData.results && (
         <div className={styles.resultContainer}>
           {quizData.quizType === "q&a" ? (
-            <>
+            <div className={styles.qa}>
               <div>Congrats! Quiz is Completed</div>
-              <img src="trophy.png" alt="Trophy" />
-              <div>{`Your Score: ${quizData.results.correctAnswers}/${quizData.questions.length}`}</div>
-            </>
+              <img src={Trophy} alt="Trophy" />
+              <div>
+                <span>Your Score: </span>
+                <span
+                  className={styles.score}
+                >{`0${resultData.results.correctQuestions} / 0${quizData.questions.length}`}</span>
+              </div>
+            </div>
           ) : (
-            <div>Thank you for participating in the poll!</div>
+            <div className={styles.poll}>
+              Thank you for participating in the poll!
+            </div>
           )}
         </div>
       )}
